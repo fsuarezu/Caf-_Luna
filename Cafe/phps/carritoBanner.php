b95c080956412146e9c8dc3c7c59d41cfdaf5e4d
fsuarezu <fsuarezu@gmail.com>
2026-06-01 23:19:26 -0400

    cambio a responsive y vista de admin

--- Cafe/phps/carritoBanner.php
+++ Cafe/phps/carritoBanner.php
@@ -1,1 +1,3 @@
-<a href="Carrito(SEGUIR EDITANDO).php" class="carrito-flotante">🛒</a>
+<a href="Carrito(SEGUIR EDITANDO).html" class="carrito-flotante">
+    <img src="imagenes/cart4.svg" width="50" height="50">
+</a>
